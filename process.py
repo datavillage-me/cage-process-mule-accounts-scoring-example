3faa125aff872a0d3e15e53d49d8ba865fae62f0
Frederic Lebeau <frederic@datavillage.me>
2024-06-22 11:37:22 +0200

    fix

--- process.py
+++ process.py
@@ -158,8 +158,7 @@ def process_score_event(evt: dict):
         query=f"SELECT * FROM read_parquet({parquet2})"
         
         try:
-            logger.info(os.environ['CURL_CA_INFO'])
-            logger.info(os.environ['CURL_CA_PATH'])
+            
             for name, value in os.environ.items():
                 print("{0}: {1}".format(name, value))
             os.environ['CURL_CA_PATH']="/etc/ssl/certs/"
